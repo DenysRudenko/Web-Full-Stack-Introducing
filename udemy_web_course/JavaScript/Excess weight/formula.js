faebad13bbcb677e5eb1e646fda18c61b1524081
DenysRudenko <heartbreathing19951014@gmail.com>
2022-03-27 17:51:04 +0000

    Excess weight:finished the task

--- udemy_web_course/JavaScript/Excess weight/formula.js
+++ udemy_web_course/JavaScript/Excess weight/formula.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+var height = prompt("Please input your height in meters")
+var weight = prompt("Please input your weight")
+
+var bodyWeightIndex = weight / (height * height);
+var doYouHaveExcessWeight = bodyWeightIndex > 28;
+
+alert("You have excess weight:" + doYouHaveExcessWeight);
