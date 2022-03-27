d5ce4166fef210b816cccf3986d2c30e931b6df9
Denys Rudenko <heartbreathing19951014@gmail.com>
2022-03-27 12:17:53 +0000

    Udemy course of web

---
+++ udemy_web_course/JavaScript/exchange/js/ex.js
@@ -0,0 +1,5 @@
+var start = prompt("Cколько долларов вы хотите поменять?");
+var dollar = 30;
+
+console.log(start);
+alert("Вы получите" + " " + start * 30 + " гривен");
---
+++ udemy_web_course/JavaScript/script.js
@@ -0,0 +1,17 @@
+//this is comment
+//var firstName = "Vasya";
+/*console.log(firstName);
+
+alert(firstName);*/
+
+//firstName =	prompt("what is your name");
+//console.log(firstName);
+
+var firstName = prompt("Enter your name");
+var sureName = prompt("Enter your surename");
+var age = prompt("Enter your age");
+var user = "Пользователь:";
+var ageConsole = "Возраст:";
+
+console.log(user + " " + firstName + " " + sureName);
+console.log(ageConsole + " " + age);
