fc4aa8adea3c2c1e9115a03c2e0267bd50729141
DenysRudenko <heartbreathing19951014@gmail.com>
2022-03-27 18:37:47 +0000

    Lesson: If/Else files created.

---
+++ udemy_web_course/JavaScript/if_else.js
@@ -0,0 +1,10 @@
+var userName = "Ivan";
+var userAge = 9;
+
+if (userAge >= 18) {
+    console.log(userName + " is adult.");
+} else if (userAge < 10) {
+    console.log(userName + "is a child");
+}
+
+console.log("Something");
