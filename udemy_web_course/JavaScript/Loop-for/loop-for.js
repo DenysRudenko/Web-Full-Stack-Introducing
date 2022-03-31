7155b99eebfafb346ab5d6c429faa904e79ccacf
DenysRudenko <heartbreathing19951014@gmail.com>
2022-03-31 23:00:35 +0100

    Lesson:Loop-for_homework

---
+++ udemy_web_course/JavaScript/Loop-for/loop-for.js
@@ -0,0 +1,22 @@
+//First assigment for homework
+for (i = 2; i <= 30; i += 2) {
+    console.log(i);
+}
+
+console.log("-".repeat(30));
+
+//Second assigment for homework
+
+for (i = 1; i <= 30; i++){
+    if (i % 7 === 0) {
+        console.log(i);
+    }
+}
+
+console.log("-".repeat(30));
+
+//Third assigment for homework
+
+for (i = 29; i > 0; i -= 2){
+    console.log(i);
+}
