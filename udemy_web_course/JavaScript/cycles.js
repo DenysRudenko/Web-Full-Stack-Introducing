83384d46992b050f569ce6618cb00a6fe52eb448
DenysRudenko <heartbreathing19951014@gmail.com>
2022-03-31 20:40:18 +0100

    Lesson: Loop_while

---
+++ udemy_web_course/JavaScript/cycles.js
@@ -0,0 +1,16 @@
+var cycle = 1;
+
+while (cycle < 10) {
+    console.log(cycle);
+    cycle++;
+}
+
+alert("Loop is finished");
+
+var string = "Hello JavaScript";
+var count = 0;
+
+while (count < string.length) {
+    console.log(string[count]);
+    count++;
+}
