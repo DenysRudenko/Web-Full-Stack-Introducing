4b4f7f357bfd4b1849698c9723232060eec8fcd5
DenysRudenko <heartbreathing19951014@gmail.com>
2022-03-31 23:38:55 +0100

    Lesson: Function

---
+++ udemy_web_course/JavaScript/function.js
@@ -0,0 +1,14 @@
+function hello() {
+    console.log("Hello!!!");
+}
+hello();
+
+function complexHello() {
+    console.log("Hello, My name is Denys ! I`m 26 years old")
+}
+complexHello();
+
+function alertHello() {
+    alert("Hello!!!");
+}
+alertHello();
