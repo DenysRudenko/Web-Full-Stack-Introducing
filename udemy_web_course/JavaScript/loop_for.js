8afa8ea1ee034f6bc3da11d0246723bf937d8aab
DenysRudenko <heartbreathing19951014@gmail.com>
2022-03-31 22:40:42 +0100

    Lesson: Loop-for

---
+++ udemy_web_course/JavaScript/loop_for.js
@@ -0,0 +1,15 @@
+/*var count = 1;
+
+while (count <= 10) {
+    count += 2;
+    console.log(count);
+}
+
+for (count = 1; count <= 10; count++){
+    console.log(count);
+}*/
+
+var helloString = "Hello for loop!";
+for (i = 0; i < helloString.length; i++) {
+    console.log(helloString[i]);
+}
