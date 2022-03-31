510de84aab8f03baed54e8b35a02da88364a79be
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-01 00:05:18 +0100

    Lesson: Functions with parametres.|Unfinished.

---
+++ udemy_web_course/JavaScript/function_p.js
@@ -0,0 +1,16 @@
+function complexHello(text) {
+    console.log(Text);
+}
+
+complexHello("Hello, my name is Denys! I am 26 years old! \nSandly to be truth!");
+
+function square(number) {
+    console.log(number * number);
+}
+square(10);
+
+function rectangularArea(width, height) {
+    console.log(width * height);
+}
+
+rectangularArea(2, 3);
