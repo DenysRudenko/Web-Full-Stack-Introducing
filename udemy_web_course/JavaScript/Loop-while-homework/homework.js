795d53bd8940263c95a2e35b2a934b7cd9a2c849
DenysRudenko <heartbreathing19951014@gmail.com>
2022-03-31 21:34:14 +0100

    Lesson: Homework-Loop_while

---
+++ udemy_web_course/JavaScript/Loop-while-homework/homework.js
@@ -0,0 +1,32 @@
+//First part of homework
+var oddNum = 0;
+
+while (oddNum < 30) {
+    oddNum++;
+    if (oddNum % 2) console.log(oddNum);
+}
+
+console.log("-".repeat(30));
+
+//Second part of homework
+var numMulti = 0;
+
+while (numMulti < 30) {
+    numMulti += 5;
+    if (numMulti % 10) console.log(numMulti);
+}
+
+console.log("-".repeat(30));
+
+//Third part of homework
+
+var numPaired = 30;
+
+while (numPaired > 0) {
+    numPaired -= 2;
+    console.log(numPaired);
+}
+
+
+
+
