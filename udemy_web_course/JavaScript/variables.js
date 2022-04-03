7a6d9d401a825b62e036d23913182e3a62691502
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-03 14:51:11 +0100

    Lesson:Variables_scope - answer and questioning.

--- udemy_web_course/JavaScript/variables.js
+++ udemy_web_course/JavaScript/variables.js
@@ -10,3 +10,38 @@ var y = 2;
 
 someFunction();
 console.log(y);
+
+console.log("-".repeat(30));
+
+// 1`st assigment.
+// Please answer what result is expected without running the code.
+
+var x = 7
+
+function someFunction(){
+    x++;
+}
+
+console.log(x);
+x++;
+console.log(x);
+
+console.log("-".repeat(30));
+
+//Expected result in the console = 7, 8.
+
+// 2`nd assigment.
+
+function someFunction() {
+    var x = 4;
+    x++;
+}
+
+function anotherFunction() {
+    console.log(x);
+}
+
+someFunction();
+anotherFunction();
+
+//Expected result n the console = Variable x is not defined.
