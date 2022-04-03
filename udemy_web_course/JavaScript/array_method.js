69ad13bdc797cf11ac7d467b3daac32404e09908
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-03 17:01:13 +0100

    Lesson: Aray_methods - Part 2

--- udemy_web_course/JavaScript/array_method.js
+++ udemy_web_course/JavaScript/array_method.js
@@ -27,3 +27,21 @@ console.log(x);
 y = names.shift();
 console.log(names);
 console.log(y);
+
+names.push("John");
+names.push("Jack");
+console.log(names);
+
+var z = names.indexOf("Jack");
+console.log(z);
+
+var toyota = ["Camry", 2010, "sedan", "black", true];
+
+var isSedan = toyota.indexOf("sedan") === -1 ? 
+console.log("Type is not sedan."):
+console.log("Type is sedan");
+
+var cars = ["Honda", "KIA", "Peugeot", "Mercedes", "Opel", "Toyota"];
+var germanCars = cars.slice(3, 5);
+console.log(germanCars);
+console.log(cars);
