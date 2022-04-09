3f96d6da59904d464f4eef29f59accefe9cf4450
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-09 22:11:41 +0100

    Lesson:DOM_images.

---
+++ udemy_web_course/JavaScript/dom_4.js
@@ -0,0 +1,20 @@
+/*var li3 = document.getElementById("thirdElement");
+
+li3.style.color = "red";
+li3.style.fontSize = "40x";
+li3.style.background = "blue"
+
+li3.classList.add("stylish"); // adding id
+li3.classList.remove("stylish") // deleting id
+*/
+
+/*
+var image = document.getElementsByTagName("img");
+image[0].getAttribute("src"); // getting the attribute from image
+image[0].setAttribute("src", "Images/taras2.jpg");
+*/
+
+var link = document.getElementsByTagName("a");
+//console.log(link[0].getAttribute("href")); // getting the attribute from image
+link[0].setAttribute("href", "http://master-of-code.com");
+
