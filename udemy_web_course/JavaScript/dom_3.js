8eaa672c17b84fd1b8894da39678dff1b576f61d
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-09 21:20:52 +0100

    Lesson:Dom_changing_attributes.

---
+++ udemy_web_course/JavaScript/dom_3.js
@@ -0,0 +1,8 @@
+var li3 = document.getElementById("thirdElement");
+/*
+li3.style.color = "red";
+li3.style.fontSize = "40x";
+li3.style.background = "blue"
+*/
+li3.classList.add("stylish"); // adding id
+li3.classList.remove("stylish") // deleting id
