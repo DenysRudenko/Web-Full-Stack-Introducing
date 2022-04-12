f4076268a72c7909e1dbbecdde9edb04d6d1ef4f
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-12 22:16:16 +0100

    Project:added_some_elements

--- udemy_web_course/jQuery/Shoping list/js/shoping-list.js
+++ udemy_web_course/jQuery/Shoping list/js/shoping-list.js
@@ -1,12 +1,20 @@
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
     $(this).toggleClass('done');
 })
 
-$('span').click(function (event) {
+$('ul').on('click', 'span', function (event) {
     event.stopPropagation();
     $(this).parent().fadeOut(function () {
         $(this).remove();
     });
+})
+
+$('input').keypress(function (event) {
+    if (event.which === 13) {
+        var itemText = $(this).val();                                     //method val
+        $(this).val('');
+        $('ul').append('<li><span>x</span> ' + itemText + '</li>');        //adding new element
+    }
 })
 
 
