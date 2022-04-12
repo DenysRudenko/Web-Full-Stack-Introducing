6a545c0ee9d42ba6810efc933a6b9d1b42a623cb
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-12 21:53:55 +0100

    Project:edited_js-removeAndfadeOut_function.

--- udemy_web_course/jQuery/Shoping list/js/shoping-list.js
+++ udemy_web_course/jQuery/Shoping list/js/shoping-list.js
@@ -3,27 +3,10 @@ $('li').click(function () {
 })
 
 $('span').click(function (event) {
-    console.log('A span is clicked!');
     event.stopPropagation();
+    $(this).parent().fadeOut(function () {
+        $(this).remove();
+    });
 })
 
-$('li').click(function () {
-    $(this).toggleClass('A li is clicked!');
-})
-
-$('ul').click(function () {
-    console.log('A ul is clicked!');
-})
-
-$('div').click(function () {
-    $(this).toggleClass('A div is clicked!');
-})
-
-$('body').click(function () {
-    $(this).toggleClass('A body is clicked!');
-})
-
-$('html').click(function () {
-    console.log('A html is clicked!');
-})
 
