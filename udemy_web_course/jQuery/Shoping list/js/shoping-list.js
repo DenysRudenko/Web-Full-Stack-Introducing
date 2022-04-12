16cb6b0cbc21b269c14a9000d141609ce71d1fa4
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-12 21:43:03 +0100

    Project:edited_js-stopPropagation()

--- udemy_web_course/jQuery/Shoping list/js/shoping-list.js
+++ udemy_web_course/jQuery/Shoping list/js/shoping-list.js
@@ -1,1 +1,29 @@
-alert("Hello");
+$('li').click(function () {
+    $(this).toggleClass('done');
+})
+
+$('span').click(function (event) {
+    console.log('A span is clicked!');
+    event.stopPropagation();
+})
+
+$('li').click(function () {
+    $(this).toggleClass('A li is clicked!');
+})
+
+$('ul').click(function () {
+    console.log('A ul is clicked!');
+})
+
+$('div').click(function () {
+    $(this).toggleClass('A div is clicked!');
+})
+
+$('body').click(function () {
+    $(this).toggleClass('A body is clicked!');
+})
+
+$('html').click(function () {
+    console.log('A html is clicked!');
+})
+
