365df6dec01f972e553dd62209c41e33556bee88
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-20 22:26:51 +0100

    PHP_2:variables.

---
+++ udemy_web_course/PHP/2_lesson.php
@@ -0,0 +1,22 @@
+<?php
+echo "Hello PHP!";
+
+$name = "John";
+echo  "My name is $name";
+$lastname = "Blackerry";
+echo $name." ".$lastname;
+
+$x = 23;
+$y = 12;
+$sum = $x + $y;
+echo "<p>$x + $y</p>";
+echo $sum;
+echo 3 + 7 / 12;
+
+$bool = true;
+echo "$bool";
+
+$variableName = "lastname";
+echo $$variableName;
+
+?>
