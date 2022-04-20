c49f6137e45f45e08fc24ef398a918221cf56504
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-20 22:40:20 +0100

    PHP_3:arrays.

---
+++ udemy_web_course/PHP/3_lesson.php
@@ -0,0 +1,23 @@
+<?php
+
+# Arrays
+
+$colors = array("red", "blue", "orange", "green");
+print_r($colors);
+
+echo $colors[3];       # looking for green.
+
+$names[0] = "Jane";
+$names[1] = "Jim";
+$names[2] = "Jack";
+
+print_r($names);
+
+$years["Jane"] = 1998;
+$years["Jare"] = 1991;
+$years["Jim"] = 1905;
+
+print_r($years);
+
+
+?>
