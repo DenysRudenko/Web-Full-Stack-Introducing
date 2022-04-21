8bdf23038c081d7d4dcae24e0572da17b23ee893
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-21 21:24:19 +0100

    PHP_7:GET.

---
+++ udemy_web_course/PHP/7_lesson.php
@@ -0,0 +1,14 @@
+<?php
+
+    print_r($_GET);
+    echo "Hello".$_GET["userName"]."!";
+
+
+?>
+
+<p>Input your name</p>
+
+<form>
+    <input type="text" name="userName">
+    <input type="submit" name="">
+</form>
