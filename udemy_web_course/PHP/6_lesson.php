e7b87c4a0f6dab12e64a0d2db3b6999a7c064fa4
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-21 21:13:09 +0100

    PHP_6:cycle_while.

---
+++ udemy_web_course/PHP/6_lesson.php
@@ -0,0 +1,21 @@
+<?php
+
+$i = 1;
+
+while ($i <= 10) {
+    $y = $i * 7;
+
+    echo $y." ";
+    $i++;
+}
+
+$colors = array("red", "orange", "yellow", "green", "blue", "indigo", "violet");
+
+$j = 0;
+
+while ($j < sizeof($colors)) {
+    echo $colors[$j];
+    $j++;
+}
+
+ ?>
