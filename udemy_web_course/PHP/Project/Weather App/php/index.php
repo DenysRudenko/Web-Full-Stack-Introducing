1d4884c7e605df689bf9427136f090df95c0918d
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-24 19:40:58 +0100

    PHP-Project:finished.

--- udemy_web_course/PHP/Project/Weather App/php/index.php
+++ udemy_web_course/PHP/Project/Weather App/php/index.php
@@ -1,30 +1,86 @@
+<?php
+
+error_reporting(E_ERROR | E_PARSE);
+
+$weather = "";
+
+$error = "";
+
+if (isset($_GET['city'])) {
+
+    $urlContent = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q=' . $_GET['city'] . '&units=metric&appid=f66e7efc9182aef5d0c5b65b52a2e5e6');
+
+    $forcastArray = json_decode($urlContent, true);
+
+    if ($forcastArray['cod'] == 200) {
+
+        $weather = 'The weather in ' . $_GET['city'] . ' is ' . $forcastArray['weather'][0]['description'];
+
+        $weather = $weather . '. The Temperature is ' . $forcastArray['main']['temp'] . '&#8451;';
+    } else {
+        $error = "The city name is incorrect, please try another name.";
+    }
+}
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 
     <title>Weather App</title>
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
+    <div class="container" id="mainDiv">
+        <h1>Weather In The City</h1>
+
+        <form>
+            <div class="form-group">
+                <label for="forCity">Input a city name</label>
+                <input class="form-control" name="city" id="city" aria-describedby="For Cast" placeholder="Enter Your City">
+            </div>
+            <button type="submit" class="btn btn-primary" id="button">
+                <span>Submit</span>
+            </button>
+        </form>
+    </div>
+
+    <div id="forecastDiv">
+
+        <?php
+        if ($weather) {
+
+            echo '<div class="alert alert-primary" role="alert">' . $weather . ' . ' . '</div>';
+        } else if ($error) {
+
+            echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+        }
+
+        ?>
+    </div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
