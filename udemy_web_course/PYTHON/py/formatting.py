666d6940c8ed903f52e96e7624a54d26ff368c57
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-25 22:59:21 +0100

    Python:Formatting.

---
+++ udemy_web_course/PYTHON/py/formatting.py
@@ -0,0 +1,18 @@
+# print(1 + 1)
+# print("1" + "1")
+
+# age = 23
+# print("Jack is " + str(age) + " years old.")
+
+# name = "Jack"
+# age = 23
+# name_and_age = "My name is {0}. I`m {1} years old!".format(name, age)
+# print (name_and_age)
+
+# week_days = "There are 7 days in a week: {}, {}, {}, {}, {}, {}, {}.".format('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+# print(week_days)
+
+float_result = 1000 / 7
+print(float_result)
+print('the result of division is {0:1.3f}'.format(float_result))
+
