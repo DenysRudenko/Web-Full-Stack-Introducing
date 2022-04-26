a5c957c14d7e7f730c3d4e73ec4e82b839cdf832
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-26 21:45:53 +0100

    Python:boolean.

---
+++ udemy_web_course/PYTHON/py/booleans.py
@@ -0,0 +1,17 @@
+# print(1 < 2)
+
+
+# # Comparison operators
+
+# print(1 == 1)
+# print(1 == 2)
+# print('hello' == 'jello')
+
+# print(1 != 1)
+# print(1 != 2)
+
+print (1 > 2)
+print (1 < 2)
+
+print(ord('a'))
+print(ord('b'))
