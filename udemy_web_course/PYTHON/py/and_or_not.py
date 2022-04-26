b8795ba9c1dc2ba5cdf8c22b16c88888aeb7be28
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-26 21:56:37 +0100

    Python:and,or,not.

---
+++ udemy_web_course/PYTHON/py/and_or_not.py
@@ -0,0 +1,23 @@
+x = 1
+y = 2
+
+# print(x > 1)
+# print(y > 1)
+
+# and, or, not
+
+print(x > 1 and y > 1)
+print(x > 1 or y > 1)
+print( not x > 1)
+
+print(True and True)
+print(True or True)
+print(not True)
+
+name = "John"
+age = 21
+is_married = False
+
+if age > 18 and is_married == False:
+    print("He is not married!")
+
