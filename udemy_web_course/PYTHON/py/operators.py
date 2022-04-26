449fbc5b57318c644bf701b0cbfecf19d8aef6fe
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-26 22:18:45 +0100

    Python:operators.

---
+++ udemy_web_course/PYTHON/py/operators.py
@@ -0,0 +1,21 @@
+x = 3
+
+if x > 3:
+    print ('x > 3')
+elif x < 3:
+    print ('x < 3')
+else:
+    print ('x == 3')
+
+day_time = "afternoon"
+
+if day_time == "morning":
+    print("monster wakes up!")
+elif day_time == "afternoon":
+    print("Monster is walking")
+elif day_time == "evening":
+    print("Monster is eating")
+elif day_time == "night":
+    print("Monster is sleeping")
+else:
+    print("Monster is doing something")
