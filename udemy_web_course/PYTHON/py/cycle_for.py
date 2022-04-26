8e6db68ce42b85038da122e6e3739d7152e97973
DenysRudenko <heartbreathing19951014@gmail.com>
2022-04-26 22:41:21 +0100

    Python:Cycle_for.

---
+++ udemy_web_course/PYTHON/py/cycle_for.py
@@ -0,0 +1,30 @@
+number_list = [1, 2, 4 , 5, 6, 7, 8, 9, 10]
+# for number in number_list:
+#     print(str(number) + ' Hola!')
+
+# for number in number_list:
+#     if number % 2 == 0:
+#         print(number)
+#     else:
+#         print("Hey!")
+
+# number_sum = 0
+
+# for number in number_list:
+#     number_sum = number_sum + number
+# print(number_sum)
+
+# greeting = "Hello Python"
+# for lettter in greeting:
+#     if lettter == "o":
+#         print(lettter)
+
+
+# for letter in "Hello Python":
+#     print("One more letter!")
+    
+tuple_list = [('a', 'b'), ('c', 'd'), ('e', 'f')]
+for item in tuple_list:
+    print(item)
+for letter_1, letter_2 in tuple_list:
+    print(letter_1, letter_2)
